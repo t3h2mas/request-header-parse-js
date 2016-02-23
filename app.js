aee680ca5a2ed1e8e43568a3ae2c71e94d97ebc5
Thomas <jtruant@gmx.com>
2016-02-22 22:52:48 -0700

    return parsed headers w/ json

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
+function parseSoftware(str) {
+  var start = str.indexOf('(');
+  var end = str.indexOf(')');
+
+  return str.slice(start+1, end)
+}
+
 app.get('/', function(req, res){
-  res.send('hello world');
+  console.dir(req.headers);
+  var ip = req.ip;
+  var lang = req.get('accept-language').split(',')[0];
+  var software = parseSoftware(req.get('user-agent'));
+  res.json({ipaddress: ip, language: lang, software: software});
 });
 
 app.listen(3000);
